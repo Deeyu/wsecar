eb5061ed961146dad42f1c05da3b0c07000a693c
daiyu <daiyu@wsecar.com>
2023-07-29 15:33:26 +0800

    feat: 同步版本更新

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 const color = useColorMode()
 
+useHead({
+  meta: [{
+    id: 'theme-color',
+    name: 'theme-color',
+    content: () => color.value === 'dark' ? '#222222' : '#ffffff',
+  }],
+})
+
 function toggleDark() {
   color.preference = color.value === 'dark' ? 'light' : 'dark'
 }
@@ -8,6 +16,6 @@ function toggleDark() {
 
 <template>
   <button class="!outline-none" @click="toggleDark">
-    <div class="dark:i-carbon-moon i-carbon-sun" />
+    <div class="i-carbon-sun dark:i-carbon-moon" />
   </button>
 </template>
